ccac06cb293666b066dc18cc724d53cc3c7a4620
AnaRojas <rojasduqueana@gmail.com>
2021-08-19 23:25:56 -0500

    channel

--- Acciones/Acciones_BTN/asgi.py
+++ Acciones/Acciones_BTN/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.layers import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Acciones_BTN.settings')
 
 application = get_asgi_application()
+
+channel_layer = get_channel_layer()
